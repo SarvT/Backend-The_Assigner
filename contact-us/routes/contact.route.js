20bdc29ded1a02b543ae59684fec451d1f912a0a
SarvT <91484197+SarvT@users.noreply.github.com>
2024-11-19 15:16:45 +0530

    Removed Day-1 directory

--- contact-us/routes/contact.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { contactUs, getContactUs } from "../controllers/contact.controller.js";
-
-const router = Router();
-
-router.route("/").get(getContactUs); //home
-router.route("/contact-us").post(contactUs); //send-msg
-
-export { router };
