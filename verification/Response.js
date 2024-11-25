86897ac5aa2e011eefebe77a79541f58d1e93a6a
SarvT <91484197+SarvT@users.noreply.github.com>
2024-11-25 13:38:57 +0530

    removed old data entry (verification/day-3)

--- verification/Response.js
+++
@@ -1,9 +0,0 @@
-class Response {
-  constructor(code, data,message = "Success") {
-    this.code = code;
-    this.data = data;
-    this.message = message;
-    this.success = code < 400;
-  }
-}
-export {Response}
